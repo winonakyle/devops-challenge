5e7be67b4384e0aa2502bea613764cb2589c8900
Winona Kyle <winonaksb@gmail.com>
2025-01-02 17:27:35 -0600

    added currentCity variable

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ function displayTemperature(response) {
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
+  let currentCity = document.querySelector("h1 #current-city");
   let city = searchInputElement.value;
 
   let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
+  currentCity.innerHTML = city;
 }
 
 function formatDate(date) {
